2db02b46253f5b5aa287cb50256d7d36103516fb
poprsp <poprsp@zoho.com>
2019-01-17 15:23:24 +0000

    Introduce threading

--- app.py
+++ app.py
@@ -3,21 +3,23 @@
 import argparse
 import sys
 
-from scrape.scrape import recurse
+from scrape.scrape import Scrape
 from scrape.wikipedia import Wikipedia
 
 
 def parse_args() -> argparse.Namespace:
     p = argparse.ArgumentParser()
-    p.add_argument("--pages", default=200, type=int)
     p.add_argument("--output-dir", required=True)
+    p.add_argument("--pages", default=200, type=int)
+    p.add_argument("--threads", default=16, type=int)
     p.add_argument("start_url")
     return p.parse_args()
 
 
 def main() -> int:
     args = parse_args()
-    recurse(Wikipedia, args.start_url, args.output_dir)
+    s = Scrape(Wikipedia, args.output_dir, args.pages, args.threads)
+    s.scrape(args.start_url)
     return 0
 
 
--- scrape/scrape.py
+++ scrape/scrape.py
@@ -1,33 +1,83 @@
 import os
+import queue
+import threading
 import urllib.parse
 import urllib.request
 
 import bs4
 
 
-def recurse(scraper: bs4.BeautifulSoup, url: str, output_dir: str) -> None:
-    components = urllib.parse.urlsplit(url)
+class Scrape:
+    def __init__(self,
+                 scraper: bs4.BeautifulSoup,
+                 output_dir: str,
+                 pages: int,
+                 threads: int) -> None:
+        self.scraper = scraper
+        self.output_dir = output_dir
+        self.pages = pages
+        self.threads = threads
 
-    with urllib.request.urlopen(url) as r:
-        scrape = scraper(r, "html.parser")
+        self.queue = queue.Queue()  # type: queue.Queue
+        self.lock = threading.Lock()
 
-        links = build_path(output_dir, "Links", components.path)
-        with open(links, "w") as f:
-            for link in scrape.links():
-                f.write("{}\n".format(link))
+    def scrape(self, url: str) -> None:
+        self.queue.put(url)
 
-        words = build_path(output_dir, "Words", components.path)
-        with open(words, "w") as f:
-            for word in scrape.words():
-                f.write("{} ".format(word))
+        threads = []
+        for _ in range(self.threads):
+            thread = threading.Thread(target=self.worker)
+            thread.start()
+            threads.append(thread)
 
+        self.queue.join()
+        for _ in range(self.threads):
+            self.queue.put(None)
 
-def build_path(output_dir: str, kind: str, path: str) -> str:
-    basename = os.path.basename(path)
-    if "." in basename:
-        raise ValueError("invalid basename: {}".format(basename))
+        for thread in threads:
+            thread.join()
 
-    directory = os.path.join(output_dir, kind)
-    os.makedirs(directory, exist_ok=True)
+    def worker(self) -> None:
+        while True:
+            url = self.queue.get()
+            if not url:
+                break
+            components = urllib.parse.urlsplit(url)
 
-    return os.path.join(directory, basename)
+            print("{}: scraping {}".format(threading.get_ident(), url))
+            with urllib.request.urlopen(url) as r:
+                scrape = self.scraper(r, "html.parser")
+
+                links = self.build_path("Links", components.path)
+                with open(links, "w") as f:
+                    for link in scrape.links():
+                        f.write("{}\n".format(link))
+                        with self.lock:
+                            if self.pages > 0:
+                                self.queue.put(self.build_url(
+                                    components.scheme,
+                                    components.netloc,
+                                    link
+                                ))
+                                self.pages -= 1
+
+                words = self.build_path("Words", components.path)
+                with open(words, "w") as f:
+                    for word in scrape.words():
+                        f.write("{} ".format(word))
+
+            self.queue.task_done()
+
+    def build_path(self, kind: str, path: str) -> str:
+        basename = os.path.basename(path)
+        if ".." in basename:
+            raise ValueError("invalid basename: {}".format(basename))
+
+        directory = os.path.join(self.output_dir, kind)
+        os.makedirs(directory, exist_ok=True)
+
+        return os.path.join(directory, basename)
+
+    @staticmethod
+    def build_url(scheme: str, netloc: str, path: str) -> str:
+        return "{}://{}/{}".format(scheme, netloc, path.lstrip("/"))
